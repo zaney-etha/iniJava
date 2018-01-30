ccbdf3551ae498ce770f87925364d89fbb5f8453
zaney-etha <zaney@live.fr>
2018-01-30 10:23:01 +0100

    myfirstprogram avec javac

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram {
+
+public static void main(String[] args) {
+// Prints out "This is my first Java program" in
+// your console
+
+System.out.println("Hello World !");
+}
+
+}
