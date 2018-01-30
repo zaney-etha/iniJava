6670a6ffbf91e4870a5b1de63537636d6517b1e8
zaney-etha <zaney@live.fr>
2018-01-30 10:59:23 +0100

    organisation

---
+++ src/prologue/launcher.java
@@ -0,0 +1,5 @@
+package prologue;
+
+public class launcher {
+
+}
---
+++ src/prologue/myFirstProgram.java
@@ -0,0 +1,12 @@
+package prologue;
+
+public class myFirstProgram {
+
+	public static void main(String[] args) {
+		// Prints out "This is my first Java program" in
+		// your console
+	
+		System.out.println("Hello World !");
+	}
+	
+}
